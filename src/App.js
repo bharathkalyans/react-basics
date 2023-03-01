a09462905879bef02fa7df94899ff7bb5b1392be
Bharath Kalyan S <sbharath35@gmail.com>
2023-03-01 10:47:58 +0530

    Working on props and Multiple components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+import "./index.css";
+import logo from "./Screenshot 2023-02-28 at 10.27.10 PM.jpg";
+
+function Logo(props) {
+  const userLogo = <img src={logo} alt="logo" style={props.dimensions} />;
+  return userLogo;
+}
 
 function App() {
+  const dimensions = {
+    height: "400px",
+    width: "600px",
+  };
   return (
-    <>
-      <Header name="Bharath" color="purple" />
-      <div>
-        <Main greet="Howdy" />
-        <Sidebar greet="Hi" />
+    <div className="App">
+      <Header />
+      <div className="content">
+        <Main />
+        <Sidebar />
       </div>
-    </>
+      <Logo dimensions={dimensions} />
+    </div>
   );
 }
 
