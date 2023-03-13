cbd0a5a53ab93d28feac4f4eecce34ac4a790a06
Bharath Kalyan S <sbharath35@gmail.com>
2023-03-13 11:23:44 +0530

    was working on cards and props

--- src/App.js
+++ src/App.js
@@ -4,11 +4,29 @@ import Tick from "./components/Tick";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const person = {
+    name: "Bharath Kalyan S",
+    url: "https://i.imgur.com/7vQD0fPs.jpg",
+    theme: {
+      backgroundColor: "black",
+      color: "pink",
+    },
+  };
+
+  const person2 = {
+    name: "Ananth S",
+    url: "https://i.imgur.com/7vQD0fPs.jpg",
+    theme: {
+      backgroundColor: "red",
+      color: "pink",
+    },
+  };
+
   return (
     <div>
-      <Heading />
-      <Tick />
-      <TodoList />
+      <TodoList person={person} />
+      <br />
+      <TodoList person={person2} />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,24 @@
-export default function TodoList() {
-    return (
-      <ul style={{
-        backgroundColor: 'black',
-        color: 'pink'
-      }}>
+export default function TodoList(props) {
+  const person = props.person;
+  console.log(person);
+  // const person = {
+  //   name: "Bharath Kalyan S",
+  //   url: "https://i.imgur.com/7vQD0fPs.jpg",
+  //   theme: {
+  //     backgroundColor: "black",
+  //     color: "pink",
+  //   }
+  // };
+
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}</h1>
+      <img src={person.url} alt={person.name} />
+      <ul>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>
         <li>Work on the alcohol-fuelled engine</li>
       </ul>
-    );
-  }
-  
+    </div>
+  );
+}
