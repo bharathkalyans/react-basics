ab7ca4000697e180ba717bfc4b58d575be765a39
Bharath Kalyan S <sbharath35@gmail.com>
2023-03-13 16:12:17 +0530

    added util class and worked on functional rendering

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import "./App.css";
-import Heading from "./components/Heading";
-import Tick from "./components/Tick";
-import TodoList from "./components/TodoList";
+
+
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li style={{ color: "blue" }}>{name} ✅</li>;
+  } else {
+    return <li>{name}</li>;
+  }
+}
+
+function MyList() {
+  const people = [
+    {
+      id: 0,
+      name: "Creola Katherine Johnson",
+      profession: "mathematician",
+    },
+    {
+      id: 1,
+      name: "Mario José Molina-Pasquel Henríquez",
+      profession: "chemist",
+    },
+    {
+      id: 2,
+      name: "Mohammad Abdus Salam",
+      profession: "physicist",
+    },
+    {
+      name: "Percy Lavon Julian",
+      profession: "chemist",
+    },
+    {
+      name: "Subrahmanyan Chandrasekhar",
+      profession: "astrophysicist",
+    },
+  ];
+
+  const chemists = people.filter((person) => person.profession === "chemist");
+
+  const listItems = chemists.map((item) => <li>{item.name}</li>);
+
+  return <ol>{listItems}</ol>;
+}
 
 function App() {
-  const person = {
-    name: "Bharath Kalyan S",
-    url: "https://i.imgur.com/7vQD0fPs.jpg",
-    theme: {
-      backgroundColor: "black",
-      color: "pink",
-    },
-  };
-
-  const person2 = {
-    name: "Ananth S",
-    url: "https://i.imgur.com/7vQD0fPs.jpg",
-    theme: {
-      backgroundColor: "red",
-      color: "pink",
-    },
-  };
-
   return (
     <div>
-      <TodoList person={person} />
-      <br />
-      <TodoList person={person2} />
+      <section>
+        <h1>Sally Ride's Packing List</h1>
+        <Item name="apple" isPacked={true} />
+        <Item name="banana" isPacked={true} />
+        <Item name="orange" isPacked={false} />
+      </section>
+      <MyList />
     </div>
   );
 }
